76274a287756f327fef178ba10f6373d775b88e2
tunishq <74109308+tunishq@users.noreply.github.com>
2022-02-06 11:16:59 +0000

    Add files via upload

---
+++ water_bottle.js
@@ -0,0 +1,29 @@
+STATUS = "";
+function preload()
+{
+    img=loadImage("water_bottle.jpg");
+}
+
+function setup ()
+{
+canvas = createCanvas(640, 420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded ()
+{
+    console.log("model loaded");
+    STATUS = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error("error");
+    }
+    console.log(results);
+}
